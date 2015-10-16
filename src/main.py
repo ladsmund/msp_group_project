89f68057e585a07a862cf0efc90972740ef008b6
Mads <madsclund@gmail.com>
2015-10-16 18:48:57 -0400

    basic matrix based rythms

--- src/main.py
+++ src/main.py
@@ -8,6 +8,17 @@ BUFFER_SIZE = 2 ** 10
 
 if __name__ == "__main__":
 
+    oscilator_list = []
+    frequencies = [200, 300, 400, 1000, 4000, 4090]
+    gains = [.1, .1, .1, .1, .01, .01]
+    rythms = []
+    rythms.append([1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0])
+    rythms.append([0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1])
+    rythms.append([1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1])
+    rythms.append([0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0])
+    rythms.append([1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1])
+    rythms.append([0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0])
+
     dac = DAC(BUFFER_SIZE)
 
     mixer = Mixer()
@@ -16,15 +27,31 @@ if __name__ == "__main__":
     dac.connect(mixer.callback)
     dac.start()
 
-    osc = Oscilator(dac.getSamplerate(), dac.getBufferSize())
-    osc.start()
-    mixer.addDevice(osc)
-
     mixer.setVolume(.8)
 
-    for frequency in range(100,1000,10):
-        print frequency
-        osc.setFreq(frequency)
+    for i in range(0, len(frequencies)):
+        f = frequencies[i]
+        g = gains[i]
+        osc = Oscilator(dac.getSamplerate(), dac.getBufferSize())
+        osc.setFreq(f, g)
+        mixer.addDevice(osc)
+        oscilator_list.append(osc)
+
+    print len(oscilator_list)
+
+    i = 0
+    while True:
+
+        oscilator_index = 0
+        for j in range(0, len(oscilator_list)):
+            rythm = rythms[j]
+            osc = oscilator_list[j]
+            if rythm[i]:
+                osc.start()
+            else:
+                osc.stop()
+
         sleep(STEP_SLEEP_INTERVAL)
 
-
+        i += 1
+        i %= 8
