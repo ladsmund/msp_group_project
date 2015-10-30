b5ed43d93b81a7256eac52f5fa0724fe45518d74
Mads <madsclund@gmail.com>
2015-10-30 14:57:03 -0400

    Clean up main file and run the rhythm a limited time

--- src/main.py
+++ src/main.py
@@ -3,59 +3,49 @@
 import sys
 from time import sleep
 
-import numpy as np
-
 from dac import DAC
 from mixer import Mixer
 
 import parser
 
-DEFAULT_BEAT_INTERVAL = 0.2
-BUFFER_SIZE = 2 ** 10
-
 if __name__ == "__main__":
 
     (speed, length, subdivision, samplerate, buffersize, instruments, rhythms, gains) = parser.parse(sys.argv[1])
 
+    beat_interval = 60. / (speed * subdivision)
     dac = DAC(buffersize)
 
     mixer = Mixer()
-    mixer.setVolume(0)
+    mixer.setVolume(1.)
 
     dac.connect(mixer.callback)
     dac.start()
 
-    mixer.setVolume(.8)
 
     for instrument in instruments:
-        mixer.addDevice(instrument)
-
-    # instrument = Sampler('./instruments/samples/un_TC-03-G1-05.wav')
-    # mixer.addDevice(instrument)
-    #
-    # for i in range(1, len(frequencies)):
-    #     f = frequencies[i]
-    #     instrument = SineSynth(dac.getSamplerate(), dac.getBufferSize())
-    #     instrument.setFreq(f)
-    #     instrument.start()
-    #     mixer.addDevice(instrument)
+        channel = mixer.addDevice(instrument)
+        # print instrument
 
     i = 0
-    while True:
+    # while True:
+    for j in range(64):
+        print "Beat"
 
         oscilator_index = 0
         for j in range(0, len(mixer.channels)):
             gain = gains[j]
-            rythm = rhythms[j]
+            rhythm = rhythms[j]
             channel = mixer.channels[j]
             channel.gain = gain[i]
-            if rythm[i]:
-                channel.device.trigger(.1)
-                # channel.mute = False
-                # else:
-                # channel.mute = True
 
-        sleep(DEFAULT_BEAT_INTERVAL)
+            print "%3i, %i: trigger: %i, gain: %0.2f" % (i,j,rhythm[i],gain[i])
+            if rhythm[i]:
+                channel.device.trigger(.05)
+
+        sleep(beat_interval)
 
         i += 1
-        i %= 8
+        i %= length
+
+    dac.stop()
+    exit()
