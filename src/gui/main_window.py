fca4129bbc72750b422cc55394205f9a412b656f
Mads <madsclund@gmail.com>
2015-11-02 20:50:58 -0500

    Fix indention in main_window

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -2,22 +2,22 @@
 
 from Tkinter import *
 
+
 class MainWindow():
+    def __init__(self, master, sequencer):
+        self.sequencer = sequencer
+        self.master = master
+        self.start_button = Button(master, text="Start")
+        self.stop_button = Button(master, text="Stop")
+        self.quit_button = Button(master, text="Quit")
+        self.start_button.config(command=self.sequencer.play)
+        self.stop_button.config(command=self.sequencer.stop)
+        self.quit_button.config(command=self._quit)
+        self.start_button.pack()
+        self.stop_button.pack()
+        self.quit_button.pack()
 
-  def __init__(self, master, sequencer):
-
-    self.sequencer = sequencer
-    self.master = master
-    self.start_button = Button(master, text = "Start")
-    self.stop_button = Button(master, text = "Stop")
-    self.quit_button = Button(master, text = "Quit")
-    self.start_button.config(command = self.sequencer.play)
-    self.stop_button.config(command = self.sequencer.stop)
-    self.quit_button.config(command = self._quit)
-    self.start_button.pack()   
-    self.stop_button.pack()   
-    self.quit_button.pack()   
-
-  def _quit(self):
-    self.master.destroy()
-    self.master.quit()
+    def _quit(self):
+        print("MainWindow: _quit")
+        self.master.destroy()
+        self.master.quit()
