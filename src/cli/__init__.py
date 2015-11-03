e8119858a622a2999ea597e943d8345789b643d0
Mads <madsclund@gmail.com>
2015-11-02 20:50:30 -0500

    move CLI implementation from main to separate module

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -1,1 +1,19 @@
 __author__ = 'mads'
+import sys
+
+def cli(sequencer):
+
+    key = None
+    try:
+        while key != 'q':
+            sys.stdout.write('>')
+            key = sys.stdin.readline().strip()
+            if key == 'p':
+                sequencer.play()
+            elif key == 's':
+                sequencer.stop()
+    except KeyboardInterrupt:
+        pass
+
+    sequencer.stop()
+
