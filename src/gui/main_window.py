b12d1be5500eef5e4d295e11627ccf1432ff7da0
Mads <madsclund@gmail.com>
2015-11-05 18:02:01 -0500

    update_visual -> toggle_visual

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -9,15 +9,15 @@ class RhythmButton(Button):
         self.sequencer = sequencer
         self.instrument = i
         self.beat = b
-        self.update_visual()
+        self.toggle_visual()
 
     def command(self):
         instrument = self.instrument
         beat = self.beat
         self.sequencer.rhythms[instrument][beat] = not self.sequencer.rhythms[instrument][beat];
-        self.update_visual()
+        self.toggle_visual()
 
-    def update_visual(self):
+    def toggle_visual(self):
         if self.sequencer.rhythms[self.instrument][self.beat]:
             self.config(text="x")
         else:
@@ -38,23 +38,16 @@ class RhythmFrame(Frame):
                 self.buttons.append(button)
                 button.grid(row=i, column=b)
 
-    def set_rhythm(self, instrument, beat):
-        # self.sequencer.rhythms[instrument][beat] = not self.sequencer.rhythms[instrument][beat]
-        self.sequencer.rhythms[instrument][beat] = 4
-        print("instrumnet: %i, beat: %i" % (instrument, beat))
-        for b in self.sequencer.rhythms[instrument]:
-            sys.stdout.write(str(b))
-        sys.stdout.write('\n')
-
-
 class MainWindow():
     def __init__(self, master, sequencer):
         self.sequencer = sequencer
         self.master = master
+
         self.control_panel = Frame(master)
         self.start_button = Button(self.control_panel, text="Start")
         self.stop_button = Button(self.control_panel, text="Stop")
         self.quit_button = Button(self.control_panel, text="Quit")
+
         self.start_button.config(command=self.sequencer.play)
         self.stop_button.config(command=self.sequencer.stop)
         self.quit_button.config(command=self._quit)
@@ -62,6 +55,7 @@ class MainWindow():
         self.stop_button.pack()
         self.quit_button.pack()
         self.control_panel.grid(row=0, column=0)
+
         self.rhythm_frame = RhythmFrame(master, sequencer)
         self.rhythm_frame.grid(row=0, column=1)
 
