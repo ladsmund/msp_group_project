0d82200292144f956272ece7ce30cbf3e66b9849
Mads <madsclund@gmail.com>
2015-11-05 19:30:42 -0500

    Add RhythmTrack frame

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -4,42 +4,46 @@ from Tkinter import Tk
 from ttk import Button, Frame
 
 
-
 class RhythmButton(Button):
-    def __init__(self, master, sequencer, i, b):
+    def __init__(self, master, track, beat):
 
-        Button.__init__(self, master, text="%i:%i" % (i, b), command=self.command)
-        self.sequencer = sequencer
-        self.instrument = i
-        self.beat = b
+        Button.__init__(self, master, text="--", command=self.command)
+        self.track = track
+        self.beat = beat
         self.toggle_visual()
 
     def command(self):
-        instrument = self.instrument
-        beat = self.beat
-        self.sequencer.rhythms[instrument][beat] = not self.sequencer.rhythms[instrument][beat];
+        self.track.rhythms[self.beat] = not self.track.rhythms[self.beat];
         self.toggle_visual()
 
     def toggle_visual(self):
-        if self.sequencer.rhythms[self.instrument][self.beat]:
+        if self.track.rhythms[self.beat]:
             self.config(text="x")
         else:
             self.config(text=" ")
 
 
-class RhythmFrame(Frame):
+class RhythmTrackFrame(Frame):
+    def __init__(self, master, track):
+        Frame.__init__(self, master)
+        self.track = track
+
+        for b in range(0, len(self.track.rhythms)):
+            button = RhythmButton(self, track, b)
+            # self.buttons.append(button)
+            button.grid(row=0, column=b)
+
+
+class SequencerFrame(Frame):
     def __init__(self, master, sequencer):
         Frame.__init__(self, master)
-
         self.sequencer = sequencer
-        self.buttons = []
-        rhythm = sequencer.rhythms[0]
 
-        for i in range(0, len(sequencer.rhythms)):
-            for b in range(0, len(rhythm)):
-                button = RhythmButton(self, sequencer, i, b)
-                self.buttons.append(button)
-                button.grid(row=i, column=b)
+        row = 0;
+        for track in sequencer.tracks:
+            RhythmTrackFrame(self, track).grid(row=row, column=0)
+            row += 1
+
 
 class MainWindow(Tk):
     def __init__(self, sequencer):
@@ -59,8 +63,8 @@ class MainWindow(Tk):
         self.quit_button.pack()
         self.control_panel.grid(row=0, column=0)
 
-        # self.rhythm_frame = RhythmFrame(self, sequencer)
-        # self.rhythm_frame.grid(row=0, column=1)
+        self.sequencer_frame = SequencerFrame(self, sequencer)
+        self.sequencer_frame.grid(row=0, column=1)
 
     def _quit(self):
         self.destroy()
