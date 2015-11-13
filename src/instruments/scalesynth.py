720ee08f18f5edf86eb72574622ed71205b58c63
Mads <madsclund@gmail.com>
2015-11-13 15:15:04 -0500

    Print played frequencies from scale synth

--- src/instruments/scalesynth.py
+++ src/instruments/scalesynth.py
@@ -4,9 +4,8 @@ from oscilator import Oscilator
 
 
 class ScaleSynth(Oscilator):
-
     def __init__(self, samplerate, bufferSize, scale):
-        self.scale = scale 
+        self.scale = scale
         Oscilator.__init__(self, samplerate, bufferSize)
         self.on = False
         self.length = 0
@@ -17,9 +16,10 @@ class ScaleSynth(Oscilator):
         frequency = self.scale.get_interval_frequency(tone)
         self.setFreq(frequency)
 
-    def trigger(self, note=1, length =.1):
+    def trigger(self, note=1, length=.1):
         self.on = True
         self.length = length
+        print "trigger. Freq: %5.1fHz" % self.frequency
         self.trigger_start = time.time()
 
     def callback(self, in_data, frame_count, time_info, status):
@@ -33,25 +33,6 @@ class ScaleSynth(Oscilator):
 
         return output_buffer
 
-
     def __str__(self):
-        string = "SineSynth\n  Frequency: %0.1f"%self.frequency
+        string = "SineSynth\n  Frequency: %0.1f" % self.frequency
         return string
-
-    # def __init__(self, frequency):
-    #     self.oscilator = Oscilator
-    #     pass
-
-    # def start(self):
-    #     pass
-
-    # def stop(self):
-    #     pass
-
-    # def callback(self, in_data, frame_count, time_info, status):
-    #     pass
-
-
-
-
-
