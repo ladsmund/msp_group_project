f298e286108b9fd80fa683f1fff6c78c492b4798
Aric Alain Werner <aric.werner@gmail.com>
2015-11-14 16:54:08 -0500

    Add polyphonic scale synth

--- src/instruments/scalesynth.py
+++ src/instruments/scalesynth.py
@@ -22,3 +22,26 @@ class ScaleSynth(Instrument):
     def __str__(self):
         string = "SineSynth\n  Frequency: %0.1f" % self.frequency
         return string
+
+class PolyphonicScaleSynth(Mixer):
+
+    def __init__(self, sample_rate, buffer_size, scale):
+        Mixer.__init__(self)
+        self.sample_rate = sample_rate
+        self.buffer_size = buffer_size
+        self.scale = scale
+        self.sub_synths = {}
+        
+    def _add_synth(self, tone):
+        synth = ScaleSynth(self.sample_rate, self.buffer_size, self.scale)
+        Mixer.add_device(self, synth)
+        self.sub_synths[tone] = synth
+
+    def on(self, tone):
+        if tone not in self.sub_synths:
+            self._add_synth(tone)
+        self.sub_synths[tone].on(tone)
+
+    def off(self, tone):
+        if tone in self.sub_synths:
+            self.sub_synths[tone].off()
