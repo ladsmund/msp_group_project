3ec0a8dcbdc566f1770d3be033ad40c6e66c8092
Mads <madsclund@gmail.com>
2015-12-03 20:08:41 -0500

    Parse arguments in homework9

--- koshka/homework9.py
+++ koshka/homework9.py
@@ -2,30 +2,40 @@
 
 from dac import DAC
 from sequencers.sequencer import Sequencer
-import instruments 
+import instruments
 
-dac = DAC(bufferSize=2048, rate=44100)
-dac.start()
-try:
-    sequencer = Sequencer(buffer_size=dac.bufferSize, sample_rate=dac.getSamplerate()) 
-    dac.connect(sequencer.callback)
-    
-    # Format: instrument id, tone, on, wait time (in "ticks")
-    score = [(0, 0, 1, 0),
-             (0, 0, 0, 1),
-             (0, 1, 1, 0),
-             (0, 1, 0, 1),
-             (0, 0, 1, 0),
-             (0, 0, 0, 1),
-             (0, 1, 1, 0),
-             (0, 1, 0, 1),
-            ]
+import argparse
 
-    instrument = instruments.parse("ScaleSynth EvenTempered 1000".split(), 
-                                   buffer_size=dac.bufferSize, 
-                                   sample_rate=dac.getSamplerate()) 
-    sequencer.add_instrument(instrument)
-    sequencer.play(score=score, block=True)
-finally:
-    dac.stop()
+DEFAULT_INSTRUMENT = "ScaleSynth EvenTempered 528"
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='MSP homework 9')
+    parser.add_argument('score', type=str)
+    parser.add_argument('--instrument', type=str, default=DEFAULT_INSTRUMENT)
+    namespace = parser.parse_args()
+
+    dac = DAC(bufferSize=2 ** 10, rate=44100)
+    dac.start()
+
+    try:
+
+        sequencer = Sequencer(buffer_size=dac.bufferSize, sample_rate=dac.getSamplerate())
+        dac.connect(sequencer.callback)
+
+        instrument = instruments.parse(namespace.instrument.split(),
+                                       buffer_size=dac.bufferSize,
+                                       sample_rate=dac.getSamplerate())
+
+        sequencer.add_instrument(instrument)
+
+        file = open(namespace.score, 'r')
+        score_string = file.read()
+        file.close()
+        score = Sequencer.parse_mono_score(score_string)
+
+        sequencer.play(score=score)
+        sequencer.stop()
+    finally:
+        dac.stop()
 
